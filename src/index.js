074ecc18991eed6367fe343d8ffc294995d58d3b
Chris Reynolds <chris@humanmade.com>
2021-02-18 15:54:21 -0700

    add main index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// ⚔️
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './assets/style.css';
+
+render(
+	<App />,
+	document.querySelector('#main')
+);
