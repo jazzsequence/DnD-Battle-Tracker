3ab9ec8422f3060e8a0df97ed9dc42aa3259f474
Chris Reynolds <chris@humanmade.com>
2021-02-19 10:50:16 -0700

    add a component to handle the save button

---
+++ src/components/SaveButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SaveButton extends React.Component {
+
+	render() {
+		return (<button id="save-all-fields" disabled>Let's go! 💥</button>)
+	}
+}
+
+export default SaveButton;
