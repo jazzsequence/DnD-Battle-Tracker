1f9b730ead46b825a176b8369960cc967d022bda
Chris Reynolds <chris@humanmade.com>
2021-02-19 15:28:51 -0700

    I don't think we need this function anymore

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -3,35 +3,6 @@ import CharacterList from './CharacterList';
 import AddCharacterForm from './AddCharacterForm';
 
 class Characters extends React.Component {
-	addCharacter = ( character ) => {
-
-		// const tmpCharButton = document.getElementById( `add-tmp-char-${id}` );
-		// const charInput = document.getElementById( `addChar-${id}` );
-		// const charName = charInput ? charInput.value : false;
-		// const dexInput = document.getElementById( `charDex-${id}` );
-		// const dexVal = dexInput ? dexInput.value : false;
-		// let hasName = false;
-
-		// if ( charName && charName !== 'undefined' && charName !== '' ) {
-		// 	// We have a name, so toggle hasName to true.
-		// 	hasName = true;
-
-		// 	// Remove the old input and button.
-		// 	charInput.remove();
-
-		// 	// Only remove the add button if we have a Dex value.
-		// 	if ( dexVal !== 'undefined' && dexVal !== '' ) {
-		// 		tmpCharButton.remove();
-		// 	} else {
-		// 		// We still need to add the dex. Change the button value to indicate that a Dex score is still needed.
-		// 		const buttonTxt = `${tmpCharButton.textContent} Dex`;
-		// 		tmpCharButton.textContent = buttonTxt;
-		// 	}
-
-		// 	buildName( thisItem, id, charName );
-		// }
-	}
-
 	buildName = ( a, b, c ) => {}
 
 	render() {
