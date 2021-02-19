15a4ef94b08ef6f9f89aa954ca26b1a940d9eca1
Chris Reynolds <chris@humanmade.com>
2021-02-19 10:39:39 -0700

    the initiative component to handle initiative

---
+++ src/components/Initiative.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import InitiativeList from './InitiativeList';
+
+class Initiative extends React.Component {
+
+	render() {
+		return (
+			<div className="initiative-order">
+				<InitiativeList />
+			</div>
+		)
+	}
+}
+
+export default Initiative;
