0e3298e4735e7efe22b5c3007ef7f3a27186cbdb
Chris Reynolds <chris@humanmade.com>
2021-02-19 10:40:03 -0700

    update the app with initiative, characters and npcs

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Header from './Header';
+import Initiative from './Initiative';
+import Characters from './Characters';
+import NPCs from './NPCs';
 
 class App extends React.Component {
 	render() {
 		return (
+			<>
 			<Header />
+			<div className="battle-tracker">
+				<Initiative />
+				<Characters />
+				<NPCs />
+			</div>
+			</>
 		)
 	}
 }
