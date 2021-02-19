e75a88f3e19fd5d8ee5df27eacde6354e59a1956
Chris Reynolds <chris@humanmade.com>
2021-02-19 15:56:05 -0700

    start building out a single character component

--- src/components/CharacterSingle.js
+++ src/components/CharacterSingle.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 class CharacterSingle extends React.Component {
 
+
+	render() {
+		const character = this.props.details;
+		const isPC = this.props.type === 'character' ? 'PC' : 'NPC';
+		return (
+			<li>
+				<form>
+					{ character.name } ({ character.init }) ({ isPC })
+					<input />
+					<button></button>
+				</form>
+			</li>
+		)
+	}
 }
 
 export default CharacterSingle;
