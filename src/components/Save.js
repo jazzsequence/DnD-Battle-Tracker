5d74d3c1022d34825dfb6d971596e41a045a82a9
Chris Reynolds <chris@humanmade.com>
2021-02-19 14:46:36 -0700

    fix classname typo

--- src/components/Save.js
+++ src/components/Save.js
@@ -7,7 +7,7 @@ class Save extends React.Component {
 		return (
 			<div className="start" id="save-fields">
 				<SaveButton />
-				<p class="description" id="save-all-fields-desc">Click the button when all characters have been added to start the battle!</p>
+				<p className="description" id="save-all-fields-desc">Click the button when all characters have been added to start the battle!</p>
 			</div>
 		)
 	}
