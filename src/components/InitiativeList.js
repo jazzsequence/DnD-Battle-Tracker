18400fd4dd3623e898e7ed504b0d6a48094d5466
Chris Reynolds <chris@humanmade.com>
2021-02-19 10:39:52 -0700

    the initiative list component to handle the initiative list

---
+++ src/components/InitiativeList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class InitiativeList extends React.Component {
+
+	render() {
+		return (
+			<ul id="initiative-list"></ul>
+		)
+	}
+}
+
+export default InitiativeList;
