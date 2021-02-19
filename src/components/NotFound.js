bcb46adb102d0976dad2435ab365131590f1d723
Chris Reynolds <chris@humanmade.com>
2021-02-19 10:07:48 -0700

    add a new router component and not found page/component

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+	<div>
+		<h2>Critical Fail!</h2>
+		<p>The page you were looking for was not found. Roll again, maybe?</p>
+	</div>
+)
+
+export default NotFound;
---
+++ src/components/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import App from './App';
+import NotFound from './NotFound';
+
+const Router = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={App} />
+			<Route component={NotFound} />
+		</Switch>
+	</BrowserRouter>
+)
+
+export default Router;
