d91bc7e53dd06fe1220047b48f570a37659740e0
Chris Reynolds <chris@humanmade.com>
2021-02-22 15:48:18 -0700

    add classes

--- src/components/DamageUpdateMessage.js
+++ src/components/DamageUpdateMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class DamageUpdateMessage extends React.Component {
 	render() {
 		return (
-			<span>{ this.props.message }</span>
+			<span id={ `hpUpdateMsg-${this.props.id}` } className="hp-update-msg">{ this.props.message }</span>
 		)
 	}
 }
