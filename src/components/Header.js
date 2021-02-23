14a1605258feafffa81ace26286bd4c2d7315790
Chris Reynolds <chris@humanmade.com>
2021-02-23 09:01:31 -0700

    add a link back to the home page in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header>
-				<h1>⚔️ Dungeons & Dragons Battle Tracker</h1>
+				<a href="/">
+					<h1>⚔️ Dungeons & Dragons Battle Tracker</h1>
+				</a>
 			</header>
 		)
 	}
