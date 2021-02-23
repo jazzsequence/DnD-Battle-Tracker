70ed8402cc18cbe15e0308b2e7ab8124ed187780
Chris Reynolds <chris@humanmade.com>
2021-02-23 10:40:54 -0700

    store state in local storage on update

--- src/components/App.js
+++ src/components/App.js
@@ -18,6 +18,10 @@ class App extends React.Component {
 	componentDidUpdate() {
 		// Handle starting the battle.
 		this.maybeUpdateBattleCanStart();
+
+		localStorage.setItem( this.props.match.params.encounterGuid, JSON.stringify( this.state ) );
+	}
+
 	}
 
 	updateCharacters = ( key, updatedCharacter ) => {
