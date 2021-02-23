cf49a44597b57bcbaec2969cd74d5b24148591be
Chris Reynolds <chris@jazzsequence.com>
2021-02-23 13:25:23 -0700

    variable button text depending on if the battle has started

--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -2,13 +2,22 @@ import React from 'react';
 
 class SaveButton extends React.Component {
 
+	getButtonText = () => {
+		if ( ! this.props.battleStarted ) {
+			return "Let's go! 💥";
+		}
+
+		return "Save Characters and Reset Battle 💾"
+	}
+
 	render() {
 		return (
 			<button
 				id="save-all-fields"
 				disabled={ ! this.props.battleCanStart }
-				onClick={ this.props.updateBattleStarted }
+				onClick={ () => {
 			>Let's go! 💥</button>
+			>{ this.getButtonText() }</button>
 		)
 	}
 }
